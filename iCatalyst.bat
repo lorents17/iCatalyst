8e866bf60889270b658d0286a3cc3eec72d1ebbe
lorents17 <lorents@outlook.com>
2015-06-06 10:36:01 +0300

    Update iCatalyst.bat

--- iCatalyst.bat
+++ iCatalyst.bat
@@ -862,7 +862,7 @@ title [Manual] %name% %version%
 1>&2 echo    For example: "C:\Images" "C:\logo.png"
 1>&2 echo  - The full paths of images should not be special characters. 
 1>&2 echo    For example: "&", "%%", "(", ")" etc.
-1>&2 echo  - The application automatically optimizes images in nested subfolders.
+1>&2 echo  - The application optimizes images in nested subfolders.
 1>&2 echo -------------------------------------------------------------------------------
 if exist "%tmppath%" 1>nul 2>&1 rd /s /q "%tmppath%"
 call:dopause & exit /b
