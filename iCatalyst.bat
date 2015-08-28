ebfd19e3cd3a1711cee8f125757ba752c2969fcd
Lorents <lorents@outlook.com>
2015-08-28 19:28:19 +0300

    Update iCatalyst.bat

--- iCatalyst.bat
+++ iCatalyst.bat
@@ -148,7 +148,7 @@ if defined outdir (
 cls
 title [Loading] %name% %version%
 echo.-------------------------------------------------------------------------------
-echo. Images are analyzing. Please wait...
+echo. Loading. Please wait...
 echo.-------------------------------------------------------------------------------
 cscript //nologo //E:JScript "%scripts%filter.js" %oparam% %outdirparam% %* 1>"%filelist%" 2>"%filelisterr%"
 
