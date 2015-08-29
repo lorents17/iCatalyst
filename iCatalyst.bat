b4a20e54e5c910c3af9183097839920b8e3b0710
Lorents <lorents@outlook.com>
2015-08-29 15:57:53 +0300

    Update iCatalyst.bat

--- iCatalyst.bat
+++ iCatalyst.bat
@@ -513,7 +513,7 @@ if %png% equ 1 (
 		set "zm=%%b"
 		set "zs=%%c"
 	)
-	pngwolfzopfli --zopfli-iter=5 --zlib-window=15 --zlib-level=!zc! --zlib-memlevel=!zm! --zlib-strategy=!zs! --max-stagnate-time=0 --max-evaluations=1 --in="%filework%" --out="%filework%" 1>nul 2>&1
+	pngwolfzopfli --zopfli-iter=5 --zopfli-maxsplit=0 --zlib-window=15 --zlib-level=!zc! --zlib-memlevel=!zm! --zlib-strategy=!zs! --max-stagnate-time=0 --max-evaluations=1 --in="%filework%" --out="%filework%" 1>nul 2>&1
 	if errorlevel 1 (call:saverrorlog "%~f2" "The image is not supported" & goto:pngfwe)
 )
 if %png% equ 2 (
