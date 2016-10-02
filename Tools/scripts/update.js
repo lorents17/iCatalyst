2b5f9fd1c2d52bbb112c4d6dbd12e62e90511d31
Lorents <lorents@outlook.com>
2016-10-03 00:37:12 +0300

    IC 2.7 beta 5

--- Tools/scripts/update.js
+++ Tools/scripts/update.js
@@ -1,8 +1,10 @@
 var args = WScript.Arguments;
 if(args.Count() != 1) WScript.Quit(1);
-var req = new ActiveXObject("Microsoft.XMLHTTP");
+var req = new ActiveXObject("Msxml2.ServerXMLHTTP");
+//var req = new ActiveXObject("Microsoft.XMLHTTP");
 if (req) {
 	req.open("GET", args(0), false);
+	req.setRequestHeader("Cache-Control", "max-age=0");
 	try {
 		req.send(null);
 		if (req.readyState == 4)
